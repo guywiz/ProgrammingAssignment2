01649be8cd15a794af764b3524e2597bfe84fcca
guywiz <guywiz@gmail.com>
2015-07-15 17:55:09 -0400

    Solved assignment 2
    Designed makeCacheMatrix and cacheSolve functions according to given
    prototypes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The following two functions are used to create a special object that stores a matrix and cache's its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+## The makeCacheMatrix function sort of wraps the given matrix into a list of functions
+## allowing to either set or get the initial matrix, and eventually set of get its inverse.
 
+makeCacheMatrix <- function(m = matrix()) {
+  inv <- NULL
+  set <- function(n) {
+    m <<- n
+    inv <<- NULL
+  }
+  get <- function() m
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## The cacheSolve function is responsible for actually computing the inverse
+## of the matrix *if it has not yet been computed*. Otherwise, it retrieves it.
+## If computed the inverse is then cached (saved permanently using the setinverse function
+## wrapped with the matrix (using the previous function).
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m) {
+  ## Return a matrix that is the inverse of 'm'
+  inv <- m$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  matrixData <- m$get()
+  inv <- solve(matrixData)
+  m$setinverse(inv)
+  inv
 }
+
+## Typical use of these functions is
+# > wrappedMatrix <- makeCacheMatrix()
+# > wrappedMatrix$set(m) -- where m is a matrix object
+# > cacheSolve(wrappedMatrix)
+## calling it a second time will show the cached value is used
+# > cacheSolve(wrappedMatrix)
